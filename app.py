6f56abea10dfdef0ca2d9e0885babcc596eea544
jackwardell <jackwardell@me.com>
2020-03-01 03:23:06 +0000

    changing the home page with some rele vant contennt

--- app.py
+++ app.py
@@ -3,9 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+class Config:
+    name = "Wardell's Antipodal Namesake Coefficient"
+
+
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    return render_template(
+        'index.html',
+        config=Config
+    )
 
 
 if __name__ == '__main__':
