6a08bd47447972a6bdefe9765c6b7a2129b97d31
jackwardell <jackwardell@me.com>
2020-03-01 12:06:59 +0000

    designing the landing page with a hero image

--- app.py
+++ app.py
@@ -1,19 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import os
+from mapbox import Geocoder
 
 app = Flask(__name__)
 
+MAPBOX_API_ACCESS_TOKEN = os.getenv("MAPBOX_API_ACCESS_TOKEN")
+
+mapbox_geocoder = Geocoder(access_token=MAPBOX_API_ACCESS_TOKEN)
+location_types = ["country", "region", "district", "locality", "place", "neighborhood"]
+
 
 class Config:
-    name = "Wardell's Antipodal Namesake Coefficient"
+    name = "Wardell's Antipodal Namesake Coefficient (WANC)"
 
 
-@app.route('/')
+@app.route("/")
 def hello_world():
-    return render_template(
-        'index.html',
-        config=Config
-    )
+    return render_template("index.html", config=Config)
 
 
-if __name__ == '__main__':
+@app.route("/api/search-location")
+def search_dropdown():
+    location = request.args.get("q")
+    query = mapbox_geocoder.forward(location, types=location_types)
+    results = query.json()["features"]
+    # return jsonify(
+    #     [
+    #         {"name": i["place_name"], "coordinates": i["geometry"]["coordinates"]}
+    #         for i in results
+    #     ]
+    # )
+    return jsonify([i["place_name"] for i in results])
+
+
+if __name__ == "__main__":
     app.run()
