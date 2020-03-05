6651fa5b82beee14c3c1cd8149ef643f062530bf
jackwardell <jackwardell@me.com>
2020-03-05 09:46:01 +0000

    restructering and adding new content in blog form

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, abort
 import os
 from mapbox import Geocoder
 from geopy.distance import distance
+from geojson import Point, Feature, LineString, FeatureCollection
 
 app = Flask(__name__)
 
@@ -15,16 +16,39 @@ class Config:
     name = "Antipodal Coefficient Calculator"
 
 
+def gen_linestring_feature(location_a, location_b, id_):
+    linestring = LineString([location_a.to_geojson(), location_b.to_geojson()])
+    return Feature(id=id_, geometry=linestring)
+
+
 class Location:
+    def __init__(self, latitude, longitude, name=None, **kwargs):
+        try:
+            self.latitude = float(latitude)
+        except ValueError:
+            raise ValueError(
+                "latitude must be must be a float "
+                "or must be able to be cast into a float"
+            )
+        assert (
+            -90.0 <= self.latitude <= 90.0
+        ), "latitude must be between -90 and 90 degrees"
+        self.anti_latitude = -self.latitude
 
-    def __init__(self, latitude, longitude):
-        assert isinstance(latitude, float), "latitude must be a float"
-        assert -90 <= latitude <= 90, "latitude must be between -90 and 90 degrees"
-        self.latitude = latitude
+        try:
+            self.longitude = float(longitude)
+        except ValueError:
+            raise ValueError(
+                "longitude must be a float " "or must be able to be cast into a float"
+            )
+        assert (
+            -180.0 <= self.longitude <= 180.0
+        ), "longitude must be between -90 and 90 degrees"
+        self.anti_longitude = 180 - self.longitude
 
-        assert isinstance(longitude, float), "longitude must be a float"
-        assert -180 <= longitude <= 180, "longitude must be between -90 and 90 degrees"
-        self.longitude = longitude
+        self.name = name
+
+        self.properties = {"title": self.name}.update(kwargs)
 
     @property
     def coordinates(self):
@@ -32,7 +56,19 @@ class Location:
 
     @property
     def antipode_coordinates(self):
-        return -self.latitude, 180 - self.longitude
+        return self.anti_latitude, self.anti_longitude
+
+    def to_geojson(self):
+        return self.longitude, self.latitude
+
+    def to_point(self):
+        return Point(self.to_geojson())
+
+    def to_feature(self, id_):
+        return Feature(id=id_, geometry=self.to_point(), properties=self.properties)
+
+    def antipode(self):
+        return self.__class__(self.anti_latitude, self.anti_longitude, self.name)
 
     @classmethod
     def from_mapbox(cls, mapbox_coordinates):
@@ -56,20 +92,22 @@ def hello_world():
         b = Location.from_mapbox(location_b)
         a_to_b_distance = distance(a.coordinates, b.coordinates)
         anti_a_to_b_distance = distance(a.antipode_coordinates, b.coordinates)
-        antipode_factor = 1 - (anti_a_to_b_distance / (anti_a_to_b_distance + a_to_b_distance))
+        antipode_factor = 1 - (
+            anti_a_to_b_distance / (anti_a_to_b_distance + a_to_b_distance)
+        )
 
     return render_template("index.html", config=Config)
 
 
 @app.route("/result")
-def results():
-    return render_template('result.html')
+def result():
+    return render_template("result.html")
 
 
 @app.route("/api/location")
 def search_dropdown():
     location = request.args.get("q")
-    query = mapbox_geocoder.forward(location, types=location_types)
+    query = mapbox_geocoder.forward(location, types=location_types, limit=5)
     results = query.json()["features"]
     return jsonify(
         [
@@ -77,10 +115,40 @@ def search_dropdown():
             for i in results
         ]
     )
-    # return jsonify([i["place_name"] for i in results])
 
 
-# @app.route('/api/geojson')
+@app.route("/api/geojson")
+def geojson_point():
+    latitude_a = request.args.get("latitude_a")
+    longitude_a = request.args.get("longitude_a")
+    title_a = request.args.get("title_a")
+
+    latitude_b = request.args.get("latitude_b")
+    longitude_b = request.args.get("longitude_b")
+    title_b = request.args.get("title_b")
+
+    if not latitude_a and longitude_a and latitude_b and longitude_b:
+        abort(404)
+    else:
+        location_a = Location(latitude_a, longitude_a, title_a)
+        location_b = Location(latitude_b, longitude_b, title_b)
+
+        return jsonify(
+            FeatureCollection(
+                [
+                    location_a.to_feature(id_='a'),
+                    location_b.to_feature(id_='b'),
+                    location_a.antipode().to_feature(id_='anti_a'),
+                    gen_linestring_feature(location_a, location_b, 'ab'),
+                    gen_linestring_feature(location_b, location_a.antipode(), 'b_anti_a'),
+                ]
+            )
+        )
+
+@app.route('/he')
+def he():
+    return render_template('index2.html')
+
 
 if __name__ == "__main__":
     app.run()
