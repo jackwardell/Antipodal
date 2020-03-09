8f61c78676e59d9d35d5ca72c6416ce92209e919
jackwardell <jackwardell@me.com>
2020-03-09 02:09:30 +0000

    tdd: building results page - adding table

--- tests/test_views.py
+++ tests/test_views.py
@@ -8,3 +8,7 @@ def test_results_view(test_client):
     response = test_client.get('/results')
     assert response.status_code == 200
     assert b"Wardell's Antipode Namesake Coefficient" in response.data
+    assert b"The Results" in response.data
+
+    assert b"<table>" and b"</table>" in response.data
+
