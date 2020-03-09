87491138a746b85b5af117ef2f066102aefc0c98
jackwardell <jackwardell@me.com>
2020-03-09 06:07:36 +0000

    tdd: building results page - adding datatables

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,14 +1,19 @@
 def test_home_view(test_client):
-    response = test_client.get('/')
+    response = test_client.get("/")
     assert response.status_code == 200
     assert b"Wardell's Antipode Namesake Coefficient" in response.data
 
 
 def test_results_view(test_client):
-    response = test_client.get('/results')
+    response = test_client.get("/results")
     assert response.status_code == 200
+    # check web page features
     assert b"Wardell's Antipode Namesake Coefficient" in response.data
     assert b"The Results" in response.data
 
-    assert b"<table>" and b"</table>" in response.data
-
+    # check for table features
+    assert b"<table id='results_table'>" and b"</table>" in response.data
+    # check data tables installed
+    assert b"dataTables.bootstrap4.min.js" in response.data
+    assert b"jquery.dataTables.js" in response.data
+    assert b"dataTables.bootstrap4.min.css" in response.data
